b8e2d66cf91c798c41ecad6ef0047a60d6cff1ba
fabreze <fcaballerourquhart@byupathway.edu>
2026-01-25 21:32:04 -0300

    W03 Assignment: Country Page Corrections Part 2

--- scripts/place.js
+++ scripts/place.js
@@ -1,6 +1,6 @@
 const temperature = document.getElementById('temperature').textContent;
 const windSpeed = document.getElementById('wind-speed').textContent;
-const windChill = document.getElementById('windChill');
+const windChill = document.getElementById('wind-chill');
 
 const temperatureValue = temperature.match(/-?\d+\.?\d*/)[0];
 const windSpeedValue = windSpeed.match(/-?\d+\.?\d*/)[0];
@@ -10,7 +10,7 @@ windChill.textContent = `${calculateWindChill(parseFloat(temperatureValue), pars
 function calculateWindChill(temp, windSpeed) {
     if (temp <= 10 && windSpeed > 4.8) {
         const windChill = 13.12 + 0.6215 * temp - 11.37 * Math.pow(windSpeed, 0.16) + 0.3965 * temp * Math.pow(windSpeed, 0.16);
-        return windChill.toFixed(2);
+        return windChill.toFixed(2) + " °C";
     }
     else{
         return "N/A";
